1b3eb75c1015851f7feed5a5b072606f2c817861
Goodkid01 <zaikinbkmkop@gmail.com>
2024-12-07 00:44:16 +0200

    2

--- main.cpp
+++ main.cpp
@@ -14,24 +14,38 @@ int main()
   for (int i=0;i<n;i++)
   {
     std::cin>>mas[i];
-   }
-  
-  std::cout<<mas[0]<<std::endl;
-  if(sum!=0)
+   
+  for (int j=2;j<=mas[i];j++)
   {
+    int a=mas[i];
+    if(a%j==0)
+    {
+      sum+=1;
+      break;
+    }   
+  }
+  }
+  //std::cout<<sum<<std::endl;
+  if(sum==n)
+  {
+    std::cout<<"11"<<std::endl;
     for(int i=0; i<n-1;i++)
+    {
       for (int j=i+1;j<n;j++)
+      {
         if(mas[i]<mas[j])
         {
           int tmp = mas[i];
           mas[i]=mas[j];
           mas[j]=tmp;
+        }   
 
         }
+    }
     for (int i=0;i<n;i++)
-    std::cin>>mas[i];
+    std::cout<<mas[i]<<std::endl;
   }
-  if(sum==0)
+  if(sum!=n)
     std::cout<<"10"<<std::endl;
 }
     
